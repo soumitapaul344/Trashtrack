7f8d2a5a274b7cb5a2f3de8073314d4e0c1f5cdd
Prapti <praptisarker0322@gmail.com>
2026-02-25 09:34:16 -0800

    change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,13 +1,6 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
 android {
     namespace = "com.example.trashtrack"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36   // <- এখানে 36 দিন
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -20,25 +13,18 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.trashtrack"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        targetSdk = 36   // <- optional, targetSdk 36 recommended
+        versionCode = 1
+        versionName = "1.0"
+
+        manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-}
-
-flutter {
-    source = "../.."
 }
